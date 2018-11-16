c176322f360f69607b69acef25e09a5f7d2944e7
ECShelton <E.C.Shelton@outlook.com>
2018-11-16 14:23:31 +0000

    Added butons to homepage

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,20 @@
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+@inject(Router)
 export class Home {
-  constructor() {
+  constructor(router) {
+    this.router = router;
     this.message = 'Hello World!';
+  }
+
+  goToMap()
+  {
+    this.router.navigateToRoute('map');
+  }
+
+  gotoVolunteer()
+  {
+    this.router.navigateToRoute('volunteer');
   }
 }
