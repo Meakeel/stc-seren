9f9a26513894aab2bc287f70e699f46287aec7f6
Paul <paul.sparkes@live.com>
2018-11-16 13:02:11 +0000

    adding in router

--- src/app.js
+++ src/app.js
@@ -2,4 +2,15 @@ export class App {
   constructor() {
     this.message = 'Hello World!';
   }
+  
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['', 'home'],       name: 'home',       moduleId: 'pages/home', nav: 0 },
+      { route: 'users',            name: 'users',      moduleId: 'users/index', nav: 1, title: 'Users' },
+      { route: 'users/:id/detail', name: 'userDetail', moduleId: 'users/detail', nav: 2 },
+      { route: 'files/*path',      name: 'files',      moduleId: 'files/index', nav: 3,    title: 'Files', href:'#files' }
+    ]);
+  }
 }
