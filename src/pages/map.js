967b91310e2c7fe1a1e715044395d07914d28962
Unknown <E.C.Shelton@outlook.com>
2018-11-17 11:13:00 +0000

    .

--- src/pages/map.js
+++ src/pages/map.js
@@ -54,28 +54,24 @@ export class Map {
     "custom": {"lat": 52.63023379346856, "lng": 1.297386894984811,
     "description": "description",
     "date" : "78234764",
-    "category": "Cafe"}
+    "category": "Cafe"},
+    "infoWindow" : {content: `
+      <div id='content'>
+      <div id='siteNotice'>
+      <h1 id="firstHeading" class="firstHeading">${title}</h1>
+      <div id="bodyContent">
+      <p>${catagory}</p>
+      <p>${description}</p>
+      <p>${date}</p>
+      </div>
+      </div>
+      <div>
+      `}
+
   });
   
     // let url = '/data.json';
     // this.dataaccess.loadMapData(url, this.mapdatacallback);
-  }
-
-  showPopup(nfo) {
-    let title = nfo.detail.marker.title;
-    let description = nfo.detail.marker.custom.description;
-    let category = nfo.detail.marker.custom.category;
-    console.log('popupclicked');
-    debugger;
-    
-    let pTitle = document.getElementsByClassName("title")[0];
-    pTitle.Text = title;
-    let pDesc = document.getElementsByClassName("description")[0];
-    pDesc.Text = description;
-    let pCat = document.getElementsByClassName("catagory")[0];
-    pCat.Text = category;
-
-    document.getElementsByClassName("popup")[0].style.display = "visable";
   }
 
   mapdatacallback(data) {
@@ -83,32 +79,5 @@ export class Map {
     console.log(data);
     
   this.markers.push(data);
-
-    // let marker = new google.maps.Marker({
-    //   position: data.myLatlng,
-    //   map: map,
-    //   title: data.title
-    // });
-
-//     marker.addListener('hover', function()
-//   {
-//     this.showPopup();
-//   });
-
-//   marker.addListener('mouseover', function() {
-//     infowindow.open(map, this);
-//     console.log('hover');
-// });
-
-// // assuming you also want to hide the infowindow when user mouses-out
-// marker.addListener('mouseout', function() {
-//     infowindow.close();
-// });
-
-//     // doesn't seem to do anything :(
-//     marker.addListener('click', function() {
-//       // see what we have to get the right marker.
-//       this.showPopup();
-//     });
   }
 }
